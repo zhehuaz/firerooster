80797a53f7d1cd77f27d970a0a2411b3ce0d4c15
Zhehua Zhang <zhehuaxiao@gmail.com>
2017-03-20 16:55:15 +0800

    Create Android proj and OpenCV env

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,11 @@
+#include <jni.h>
+#include <string>
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_me_zhehua_firerooster_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
