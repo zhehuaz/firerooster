81ec04089981c793e6bc2f5d35ca6201b622f47d
Zhehua Zhang <zhehuaxiao@gmail.com>
2017-03-30 23:18:22 +0800

    Use OpenCV to obtain frames.

--- app/src/main/java/me/zhehua/firerooster/MainActivity.java
+++ app/src/main/java/me/zhehua/firerooster/MainActivity.java
@@ -1,15 +1,94 @@
 package me.zhehua.firerooster;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
+//    static {
+//        if (!OpenCVLoader.initDebug()) {
+//            Log.e("OpenCV", "OpenCV init error!");
+//        }
+//    }
+
+    JavaCameraView cameraView;
+    ImageView imageView;
+    private final static String TAG = "MainActivity";
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case BaseLoaderCallback.SUCCESS:
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    cameraView.enableView();
+                break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallback);
+        cameraView.enableView();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (cameraView != null) {
+            cameraView.disableView();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cameraView = (JavaCameraView) findViewById(R.id.cv_main);
+        cameraView.setVisibility(View.VISIBLE);
+        cameraView.setCvCameraViewListener(this);
+
+        imageView = (ImageView) findViewById(R.id.iv_preview);
     }
 
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    Bitmap bitmap = Bitmap.createBitmap(640, 480, Bitmap.Config.ARGB_8888);
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        Utils.matToBitmap(inputFrame.gray(), bitmap);
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                imageView.setImageBitmap(bitmap);
+            }
+        });
+
+        return null;
+    }
 }
+
